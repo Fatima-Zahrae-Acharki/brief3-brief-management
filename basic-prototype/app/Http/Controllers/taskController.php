b9380e750f307e11c8c72723bbb4c56a3aec794d
Fatima Zahrae Acharki <fatimazahraeacharki14@gmail.com>
2022-11-10 17:01:25 +0100

    need to add tasks and finish that

--- basic-prototype/app/Http/Controllers/taskController.php
+++ basic-prototype/app/Http/Controllers/taskController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\task;
 use Illuminate\Http\Request;
 
 class taskController extends Controller
 {
-    //
+    public function viewTask(){
+        return view('addTask');
+    }
+
+    public function allTasks(){
+        return view('showTask');
+    }
+
+    public function addTask(Request $req){
+        $task = new task();
+        $task->name = $req->bName;
+        $task->start_date = $req->start_date;
+        $task->end_date = $req->end_date;
+        $task->description = $req->description;
+        $task->save();
+        return redirect('showTask');
+    }
+
+    public function showTasks(){
+        $data = task::All();
+        return view('showTask',compact('data'));
+    }
 }
